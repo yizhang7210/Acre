e42ab1992428414bed83792f2d3b4911299e052b
Yi Zhang <yi.zhang7210@gmail.com>
2017-12-28 20:38:25 -0500

    Deploy both server and worker to AWS

--- server/deploy/deploy.sh
+++ server/deploy/deploy.sh
@@ -1,3 +1,4 @@
 bash $(pwd)/deploy/setup-eb.sh
-echo 1 | eb init Acre --region us-east-2 # n is for not use code commit
+echo 1 | eb init Acre --region us-east-2 # 1 is for default environment. Doesn't actually matter.
 eb deploy acre-dev-worker
+eb deploy acre-dev
